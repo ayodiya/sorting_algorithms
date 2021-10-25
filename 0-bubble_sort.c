5cdbfaf40f1cf69c3dbd97aee145e17fce301223
ayodiya <devayodiya@gmail.com>
2021-10-25 09:12:23 -0700

    push task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,49 @@
-#include <stdio.h>
 #include "sort.h"
 
 
 /**
- * bubble_sort - bubble sort algorithm
- * @array: pointer to array to sort
- * @size: number of elements
-*/
-
-void bubble_sort()
+ * swap - swaps two integers in an array
+ * @a: first number
+ * @b: second number
+ */
+void swap(int *a, int *b)
 {
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	int len = 9;
+	int tmp = *a;
 
-	for (int i = 0; i < len; i++)
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort -  an implementation of bubble sort
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j = 0;
+	unsigned int swapped = 0;
+
+	if (!array || size < 2)
+		return;
+
+	while (j < size)
 	{
-		printf(array[i]);
+		i = 0;
+		while (i < size - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array + i, array + i + 1);
+				swapped = 1;
+				print_array(array, size);
+			}
+			else
+				i++;
+		}
+		if (!swapped)
+			return;
+		j++;
 	}
-
 }
